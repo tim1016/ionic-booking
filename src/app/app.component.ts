55e974bf8461f03b47ae002abaf2e33e5079b79c
Inkant Awasthi <10158214+tim1016@users.noreply.github.com>
2020-03-24 20:17:07 -0500

    Added auth guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(private platform: Platform, private splashScreen: SplashScreen, private statusBar: StatusBar) {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.initializeApp();
   }
 
@@ -20,5 +28,8 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
-  onLogout = () => {};
+  onLogout = () => {
+    this.authService.logout();
+    this.router.navigateByUrl('/auth');
+  };
 }
--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+  styleUrls: ['./auth.page.scss']
 })
 export class AuthPage implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  onLogin = () => {
+    this.authService.login();
+    this.router.navigateByUrl('/places/tabs/discover');
+  };
+  onLogout = () => {
+    this.authService.logout();
+  };
 }
