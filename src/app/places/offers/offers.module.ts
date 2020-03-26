c93dc972f4da8ff4632c9148683f6e0e2077d8fe
Inkant Awasthi <10158214+tim1016@users.noreply.github.com>
2020-03-26 11:14:10 -0500

    Added offer item

--- src/app/places/offers/offers.module.ts
+++ src/app/places/offers/offers.module.ts
@@ -9,6 +9,7 @@ import { OffersPageRoutingModule } from './offers-routing.module';
 import { OffersPage } from './offers.page';
 import { NewOfferPageModule } from './new-offer/new-offer.module';
 import { NewOfferPageRoutingModule } from './new-offer/new-offer-routing.module';
+import { OfferItemComponent } from './offer-item/offer-item.component';
 
 @NgModule({
   imports: [
@@ -19,6 +20,6 @@ import { NewOfferPageRoutingModule } from './new-offer/new-offer-routing.module'
     NewOfferPageModule,
     NewOfferPageRoutingModule
   ],
-  declarations: [OffersPage]
+  declarations: [OffersPage, OfferItemComponent]
 })
 export class OffersPageModule {}
