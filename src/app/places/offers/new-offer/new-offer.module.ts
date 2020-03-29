dfe3af7feccdd5b097c7ccec5ac0114a90e3ea56
Inkant Awasthi <10158214+tim1016@users.noreply.github.com>
2020-03-29 14:11:45 -0500

    Added new offer logic

--- src/app/places/offers/new-offer/new-offer.module.ts
+++ src/app/places/offers/new-offer/new-offer.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,12 +9,7 @@ import { NewOfferPageRoutingModule } from './new-offer-routing.module';
 import { NewOfferPage } from './new-offer.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    NewOfferPageRoutingModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule, IonicModule, NewOfferPageRoutingModule],
   declarations: [NewOfferPage]
 })
 export class NewOfferPageModule {}
--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
   templateUrl: './new-offer.page.html',
-  styleUrls: ['./new-offer.page.scss'],
+  styleUrls: ['./new-offer.page.scss']
 })
 export class NewOfferPage implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor() {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, { updateOn: 'blur', validators: [Validators.required] }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      price: new FormControl(null, { updateOn: 'blur', validators: [Validators.required, Validators.min(1)] }),
+      dateFrom: new FormControl(null, { updateOn: 'blur', validators: [Validators.required] }),
+      dateTo: new FormControl(null, { updateOn: 'blur', validators: [Validators.required] })
+    });
   }
 
+  onCreateOffer() {
+    console.log(this.form);
+  }
 }
